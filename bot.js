23437237ccdd30477faf8bb458177e9cfaf5df26
MarceloGamerXDT <58835348+MarceloGamerXDT@users.noreply.github.com>
2019-12-13 01:23:01 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client()
+
+client.prefix = config.prefix;
+
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)){
+        return message.reply("Olá, meu prefixo é ''**hcr.**!''")}
+    if(!message.content.startsWith(config.prefix)) return;
+
+let args = message.content.split(" ").slice(1);
+let command = message.content.split(" ")[0];
+command = command.slice(config.prefix.length);
+  try {
+      let commandFile = require(`./commands/${command}.js`);
+      delete require.cache[require.resolve(`./commands/${command}.js`)];
+      return commandFile.run(client, message, args);
+  } catch (err) {
+        console.error("Erro:" + err)
+  }
+})
+
+client.on("ready", () => {
+    console.log(`Bot foi iniciado com, ${client.users.size} usuários, ${client.guilds.size} servidores, ${client.channels.size} canais.`)
+
+    let messages = [`Assistindo ${client.users.size} pessoas`,
+                    `Hype City Roleplay`]
+
+    setInterval(() => {
+        let randomMessages = Math.floor(Math.random() * (messages.length - 1) + 1)
+        client.user.setActivity(messages[randomMessages])
+    }, 10000)
+    //0 = Transmitindo
+    //1 = Ouvindo
+    //2 = Assistindo
+})
+
+client.login(config.token)
